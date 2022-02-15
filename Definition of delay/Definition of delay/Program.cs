003fcf671c7133e920226c651453aabc43c1a60f
Александр Костюченко <kostAM96@yandex.ru>
2022-02-15 15:33:38 +0300

    12

--- Definition of delay/Definition of delay/Program.cs
+++ Definition of delay/Definition of delay/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Definition_of_delay
 {
@@ -7,7 +8,9 @@ namespace Definition_of_delay
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Warehous warehous = new Warehous();
+            warehous.ShowAllEat();
+            warehous.CheckingExpirationDate();
         }
     }
 
@@ -18,15 +21,39 @@ namespace Definition_of_delay
         public Warehous()
         {
             _eat = new List<Eat>();
+            CreateEat();
+        }
+
+        public void ShowAllEat()
+        {
+            Console.WriteLine("Вся имеющаяся еда");
+            foreach (var eat in _eat)
+                eat.ShowInfo();
+        }
+
+        public void CheckingExpirationDate()
+        {
+            int currentYear = 2022;
+
+            var suitableEat = _eat.Where(eat => eat.ExpirationDate + eat.YearProduction <= currentYear);
+
+            Console.WriteLine("Пригодная для употребления еда");
+
+            foreach (var eat in suitableEat)
+            {
+                Console.Write($"Осталось {currentYear-(eat.ExpirationDate+eat.YearProduction)} лет до истечения срока годности.  ");
+                eat.ShowInfo();
+            }
         }
 
         private void CreateEat()
         {           
-            CreateHandler(10, "Суровая тушонка", 40);
-            CreateHandler(7, "Деревенская", 14);
+            AddEatToList(10, "Суровая", 40);
+            AddEatToList(7, "Деревенская", 14);
+            AddEatToList(16, "Элитная", 42);
         }
 
-        private void CreateHandler(int eatCount, string eatName, int expirationTime)
+        private void AddEatToList(int eatCount, string eatName, int expirationTime)
         {
             for (int i = 0; i < eatCount; i++)
             {
@@ -41,14 +68,19 @@ namespace Definition_of_delay
         private Random _random;
 
         public int YearProduction { get;private set; }
-        public int ExpirationTime { get;private set; }
+        public int ExpirationDate { get;private set; }
 
         public Eat(string name, int expirationTime)
         {
             _random = new Random();
             _name = name;
-            ExpirationTime = expirationTime;
+            ExpirationDate = expirationTime;
             CreateYearProduction();
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Тушонка '{_name}' , срок годности {ExpirationDate} , сделана в {YearProduction} году");
         }
 
         private void CreateYearProduction()
